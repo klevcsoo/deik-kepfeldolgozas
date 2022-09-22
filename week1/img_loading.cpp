0b7e523317407c4ae9ebabfa4a7e4bc384f1db25
Keve Lajsz <kevelajsz@icloud.com>
2022-09-22 17:13:09 +0200

    Added image manipulation functions

--- week1/img_loading.cpp
+++ week1/img_loading.cpp
@@ -6,25 +6,47 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 
+void brighten_grayscale(const cv::Mat& in_img, cv::Mat& out_img) {
+    out_img = in_img + 100;
+}
+
+void invert_grayscale(const cv::Mat& in_img, cv::Mat& out_img) {
+    out_img = 255 - in_img;
+}
+
+void brighten_colour(const cv::Mat& in_img, cv::Mat& out_img) {
+    out_img = in_img + cv::Scalar(100, 100, 100);
+}
+
+void invert_colour(const cv::Mat& in_img, cv::Mat& out_img) {
+    out_img = cv::Scalar(255, 255, 255) - in_img;
+}
+
 int main() {
     std::cout << "OpenCV version: " << cv::getVersionString() << std::endl;
 
     std::string p = "/Users/klevcsoo/Developer/deik-kepfeldolgozas/week1/testimg.jpg";
-    cv::Mat img = cv::imread(p);
+    cv::Mat img = cv::imread(p, cv::IMREAD_COLOR);
     std::cout << "Image resolution: " << img.cols << "x" << img.rows << std::endl;
 
-    {
-        using namespace cv;
-        if (img.type() == CV_8UC1) {
-            std::cout << "8-bit monochrome" << std::endl;
-        } else if (img.type() == CV_8UC3) {
-            std::cout << "24-bit RGB" << std::endl;
-        } else {
-            std::cout << img.type() << std::endl;
-        }
+    if (img.type() == CV_8UC1) {
+        std::cout << "8-bit monochrome" << std::endl;
+    } else if (img.type() == CV_8UC3) {
+        std::cout << "24-bit RGB" << std::endl;
+    } else {
+        std::cout << img.type() << std::endl;
     }
 
-    cv::imshow("OpenCV picture", img);
+    cv::imshow("Normal image", img);
+
+    cv::Mat img_b;
+    brighten_colour(img, img_b);
+    cv::imshow("Brightened image", img_b);
+
+    cv::Mat img_i;
+    invert_colour(img, img_i);
+    cv::imshow("Inverted image", img_i);
+
     cv::waitKey();
 
     return 0;
