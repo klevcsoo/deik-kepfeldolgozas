d7a0c4605502a76fad085bdea359604e57e3eeba
Keve Lajsz <kevelajsz@icloud.com>
2022-09-29 16:14:45 +0200

    Week 2 done

--- week2/montage.cpp
+++ week2/montage.cpp
@@ -3,15 +3,20 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-void copy_foreground(const cv::Mat& fg, cv::Mat&  bg) {
-    cv::resize(bg, bg, fg.size());
+// For the orange1.jpg
+void copy_foreground(const cv::Mat& src, cv::Mat& dst, const int type = CV_8UC3) {
+    if (dst.empty()) {
+        dst.create(src.size(), type);
+        dst.setTo(0);
+    } else {
+        cv::resize(dst, dst, src.size());
+    }
 
-    const int threshold = 200;
-    for (int i = 0; i < fg.rows; i++) {
-        for (int j = 0; j < fg.cols; j++) {
-            const auto& c = fg.at<cv::Vec3b>(i, j);
-            if (c[0] < threshold || c[1] < threshold || c[2] < threshold) {
-                bg.at<cv::Vec3b>(i, j) = c;
+    for (int i = 0; i < src.rows; i++) {
+        for (int j = 0; j < src.cols; j++) {
+            const auto& c = src.at<cv::Vec3b>(i, j);
+            if (c[2] > c[0] && c[2] > c[1]) {
+                dst.at<cv::Vec3b>(i, j) = c;
             }
         }
     }
@@ -20,24 +25,24 @@ void copy_foreground(const cv::Mat& fg, cv::Mat&  bg) {
 int main() {
     std::string p_bg = "/Users/klevcsoo/Developer/"
                        "deik-kepfeldolgozas/week2/background.jpg";
-    std::string p_k = "/Users/klevcsoo/Developer/deik-kepfeldolgozas/week2/kurama.jpg";
+    std::string p_o = "/Users/klevcsoo/Developer/deik-kepfeldolgozas/week2/orange1.jpg";
 
     cv::Mat img_bg = cv::imread(p_bg, cv::IMREAD_COLOR);
-    cv::Mat img_k = cv::imread(p_k, cv::IMREAD_COLOR);
+    cv::Mat img_o = cv::imread(p_o, cv::IMREAD_COLOR);
 
     if (img_bg.empty()) {
         std::cout << "Error: Missing background" << std::endl;
     }
-    if (img_k.empty()) {
+    if (img_o.empty()) {
         std::cout << "Error: Missing kurama" << std::endl;
     }
 
 //    cv::imshow("Background", img_bg);
 //    cv::waitKey();
-//    cv::imshow("Kurama", img_k);
+//    cv::imshow("Kurama", img_o);
 //    cv::waitKey();
 
-    copy_foreground(img_k, img_bg);
+    copy_foreground(img_o, img_bg);
     cv::imshow("Result", img_bg);
     cv::waitKey();
 
