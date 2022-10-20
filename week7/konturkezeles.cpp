264ef0afedd4cfbb05bd7d75dabd515f3d719432
Keve Lajsz <kevelajsz@icloud.com>
2022-10-20 17:37:15 +0200

    Week 7: Cheese area calculations

--- week7/konturkezeles.cpp
+++ week7/konturkezeles.cpp
@@ -1,48 +1,44 @@
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "cert-msc50-cpp"
 
-#include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-cv::Scalar rand_scalar() {
-    return {
-            static_cast<double>(rand() & 255),
-            static_cast<double>(rand() & 255),
-            static_cast<double>(rand() & 255)
-    };
-}
-
 int main() {
-    cv::Mat img = cv::imread("/Users/klevcsoo/Developer/deik-kepfeldolgozas/week7/objektumok.png");
-    cv::imshow("img", img);
+    cv::Mat img = cv::imread("/Users/klevcsoo/Developer/deik-kepfeldolgozas/week7/sajt.png");
 
-    cv::Mat gray, bin_img;
+    cv::Mat gray, mask;
     cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
 
-    cv::threshold(gray, bin_img, 120, 255, cv::THRESH_BINARY);
-    cv::medianBlur(bin_img, bin_img, 5);
+    cv::threshold(gray, mask, 248, 255, cv::THRESH_BINARY_INV);
+    cv::medianBlur(mask, mask, 5);
 
     std::vector<std::vector<cv::Point>> cont;
-    cv::findContours(bin_img, cont, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
-    std::cout << "Kontúrok száma: " << cont.size() << std::endl;
+    std::vector<cv::Vec4i> hier;
+    cv::findContours(mask, cont, hier, cv::RETR_TREE, cv::CHAIN_APPROX_NONE);
+
     cv::Mat dest = img.clone();
+    double cheese_area = 0, holes_area = 0;
     for (int i = 0; i < cont.size(); i++) {
-//        cv::drawContours(dest, cont, i, rand_scalar(), 2);
-
-        std::vector<cv::Point> cont_approx;
-        cv::approxPolyDP(cont[i], cont_approx, 10, true);
-        cv::polylines(dest, cont_approx, true, rand_scalar(), 2);
-
-        cv::putText(
-                dest, std::to_string(cont_approx.size()), cont[i][0], cv::FONT_HERSHEY_SIMPLEX, 1,
-                cv::Scalar(255, 255, 255)
-        );
+        if (hier[i][3] == -1) {
+            cheese_area += cv::contourArea(cont[i]);
+            cv::drawContours(dest, cont, i, cv::Scalar(0, 255, 0), 2);
+        } else {
+            holes_area += cv::contourArea(cont[i]);
+            cv::drawContours(dest, cont, i, cv::Scalar(255, 0, 0), 2);
+        }
     }
+    cv::putText(
+            dest, "cheese: " + std::to_string(cheese_area), cv::Point(20, 20),
+            cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0), 1
+    );
+    cv::putText(
+            dest, "holes " + std::to_string(holes_area), cv::Point(20, 40),
+            cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0), 1
+    );
 
-    cv::imshow("mask", bin_img);
-    cv::imshow("dest", dest);
+    cv::imshow("cheese", dest);
 
     cv::waitKey();
     return 0;
