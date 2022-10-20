791d9720782bef6ec2ba31974eb2a299d0a87bf0
Keve Lajsz <kevelajsz@icloud.com>
2022-10-20 16:39:36 +0200

    Week 7: Outline for dog

--- week7/konturkezeles.cpp
+++ week7/konturkezeles.cpp
@@ -1,21 +1,28 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-msc50-cpp"
+
+#include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
 int main() {
-    cv::Mat img = cv::imread("/Users/klevcsoo/Developer/deik-kepfeldolgozas/week7/sajt.png");
+    cv::Mat img = cv::imread("/Users/klevcsoo/Developer/deik-kepfeldolgozas/week5/dog.jpg");
     cv::imshow("img", img);
 
     cv::Mat gray, bin_img;
     cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
 
-    cv::threshold(gray, bin_img, 248, 255, cv::THRESH_BINARY_INV);
+    cv::threshold(gray, bin_img, 120, 255, cv::THRESH_BINARY);
     cv::medianBlur(bin_img, bin_img, 5);
 
     std::vector<std::vector<cv::Point>> cont;
     cv::findContours(bin_img, cont, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+    std::cout << "Kontúrok száma: " << cont.size() << std::endl;
     cv::Mat dest = img.clone();
-    cv::drawContours(dest, cont, -1, cv::Scalar(0, 255, 0), 2);
+    for (int i = 0; i < cont.size(); i++) {
+        cv::drawContours(dest, cont, i, cv::Scalar(rand() & 255, rand() & 255, rand() & 255), 2);
+    }
 
     cv::imshow("mask", bin_img);
     cv::imshow("dest", dest);
@@ -23,3 +30,5 @@ int main() {
     cv::waitKey();
     return 0;
 }
+
+#pragma clang diagnostic pop
